79c7953909eee0f93d21ef02dded583afa7f6e32
maxkambek <maxkambekxolbekov515@gmail.com>
2022-07-28 15:49:55 +0500

    5

--- bin/if05.dart
+++ bin/if05.dart
@@ -8,4 +8,21 @@
         c: integer
     returns:
         integer: the number of negative numbers in the given numbers*/
-void main() {}
+
+int func(int a, int b, int c) {
+  int n = 0;
+  if (a < 0) {
+    n += 1;
+  }
+  if (b < 0) {
+    n += 1;
+  }
+  if (c < 0) {
+    n += 1;
+  }
+  return n;
+}
+
+void main() {
+  print(func(-1, -2, 3));
+}
