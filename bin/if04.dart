13b66fee4e80495b41402b1d811d0be08bc6bc81
maxkambek <maxkambekxolbekov515@gmail.com>
2022-07-28 15:34:12 +0500

    05

--- bin/if04.dart
+++ bin/if04.dart
@@ -8,4 +8,21 @@
         c: integer
     returns:
         integer: the number of positive numbers in the given numbers */
-void main() {}
+
+int func(int a, int b, int c) {
+  int f = 0;
+  if (a > 0) {
+    f += 1;
+  }
+  if (b > 0) {
+    f += 1;
+  }
+  if (c > 0) {
+    f += 1;
+  }
+  return f;
+}
+
+void main() {
+  print(func(2, -3, -4));
+}
