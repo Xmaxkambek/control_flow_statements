11ad9d3c7e9c18d0b97df08cbfd0be695980de86
maxkambek <maxkambekxolbekov515@gmail.com>
2022-07-29 11:47:57 +0500

    08

--- bin/if08.dart
+++ bin/if08.dart
@@ -10,5 +10,38 @@
         a: integer
     Returns:
         string: the message to print */
+String func(int number) {
+  if (number % 2 == 0) {
+    if (number > 99) {
+      if (number < 1000) {
+        return 'three-digit even number';
+      }
+    }
+  }
+  if (number % 2 == 1) {
+    if (number > 99) {
+      if (number < 1000) {
+        return 'three-digit odd number';
+      }
+    }
+  }
+  if (number % 2 == 0) {
+    if (number > 9) {
+      if (number < 100) {
+        return 'two-digit even number';
+      }
+    }
+  }
+  if (number % 2 == 1) {
+    if (number > 9) {
+      if (number < 100) {
+        return 'two-digit odd number';
+      }
+    }
+  }
+  return '';
+}
 
-void main() {}
+void main() {
+  print(func(568));
+}
